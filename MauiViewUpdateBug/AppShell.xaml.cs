c4a85b104202be403b8c5886aa615901fd1b7a10
Julian Ewers <ewerspej@tcd.ie>
2022-10-07 11:37:29 +0200

    added bug repro code

---
+++ MauiViewUpdateBug/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiViewUpdateBug;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiViewUpdateBug/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace MauiViewUpdateBug;
+
+public partial class MainPage : ContentPage
+{
+    public int Clicks { get; set; }
+
+    public MainPage()
+    {
+        InitializeComponent();
+        BindingContext = this;
+    }
+
+    protected override void OnSizeAllocated(double width, double height)
+    {
+        base.OnSizeAllocated(width, height);
+
+        VisualStateManager.GoToState(MyButton, width > height ? "Landscape" : "Portrait");
+    }
+
+    private void MyButton_OnClicked(object sender, EventArgs e)
+    {
+        Clicks++;
+        OnPropertyChanged(nameof(Clicks));
+    }
+}
+
